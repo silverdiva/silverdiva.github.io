96738eb601f82f8cb88ca83614faa15d280f5aa6
silverdiva <silverdiva4u@gmail.com>
2019-06-21 05:53:10 -0700

    updates to topics 11, 12, 13

--- scripts/08.js
+++ scripts/08.js
@@ -3,103 +3,80 @@ Topic #08 Triggering CSS3 with (Vanilla) JavaScript
 */
 //Visibility Transitions code
 // makes an element visible
-var show = function (elem) {
-
+var show = function (element) {
 	// gets the element height 
 	var getHeight = function () {
-		elem.style.display = 'block'; 
-		var height = elem.scrollHeight + 'px'; 
-		elem.style.display = ''; 
+		element.style.display = 'block';
+		var height = element.scrollHeight + 'px';
+		element.style.display = '';
 		return height;
 	};
-// makes height "invisible" or hides the element, sets max height
-	var height = getHeight(); 
-	elem.classList.add('is-visible');
-	elem.style.height = height; 
+	// makes height "invisible" or hides the element, sets max height
+	var height = getHeight();
+	element.classList.add('is-visible');
+	element.style.height = height;
 	window.setTimeout(function () {
-		elem.style.height = '';
-	}, 1200);//sets the speed of the transition
-
+		element.style.height = '';
+	}, 1200); //sets the speed of the transition
 };
-
 // makes element invisible
-var hide = function (elem) {
+var hide = function (element) {
 	// changes the element height back to 0
-	elem.style.height = elem.scrollHeight + 'px';
-
+	element.style.height = element.scrollHeight + 'px';
 	// Set the height back to 0
 	window.setTimeout(function () {
-		elem.style.height = '0';
+		element.style.height = '0';
 	}, 1);
-
 	// hide the element when transition completes
 	window.setTimeout(function () {
-		elem.classList.remove('is-visible');
+		element.classList.remove('is-visible');
 	}, 1200);
-
 };
-
 // Toggle visibility / invisibility of the element
 var magic = function (elem, timing) {
-
 	// If visible - hide it
-	if (elem.classList.contains('is-visible')) {
-		hide(elem);
+	if (element.classList.contains('is-visible')) {
+		hide(element);
 		return;
 	}
-
 	// else show it
-	show(elem);
-	
+	show(element);
 };
-
 // add event listener 
 document.addEventListener('click', function (event) {
-
 	// if class = 'magic' then that's the one we want
 	if (!event.target.classList.contains('magic')) return;
 	event.preventDefault();
-
 	var content = document.querySelector(event.target.hash);
 	if (!content) return;
-
 	// show the magic (toggle between visible and invisible)
 	magic(content);
-
 }, false);
 
 
 // */*/*/* Animated Size & Color Transitions code */*/*/* //
 //changes the div colors
-document.getElementById("div2").style.backgroundColor = "white";
-
-document.getElementById("div1").style.color = "red";
-
-//changes the background color
-document.body.style.backgroundColor = "lightblue";
+// triggered by the makeSmaller() function when clicking the "return text to normal" button
+document.getElementById("change").style.color = "red"; //text changes to red
+document.body.style.backgroundColor = "lightblue"; //background color changes from yellow to lt blue
 
 //adds the RATE (i.e. 2s) of the div and background color change
-function myFunction() {
-	document.getElementById("div1").style.WebkitTransition = "all 2s";
-	document.getElementById("div1").style.transition = "color 2s, background-color 5s, font-size 10s";
+function makeSmaller() {
+	document.getElementById("change").style.WebkitTransition = "all 2s";
+	document.getElementById("change").style.transition = "color 2s, background-color 5s, font-size 10s";
 	document.body.style.transition = "background-color 2s";
-
-	document.getElementById("div1").style.color = "blue";
-	document.getElementById("div1").style.backgroundColor = "white";
-	document.getElementById("div1").style.fontSize = "20%";
-
-	document.body.style.backgroundColor = "yellow";
+	document.getElementById("change").style.fontSize = "20%"; //makes text shrink
+	document.body.style.backgroundColor = "yellow"; //makes background color yellow
 }
-//reverts back to the original color settings
-function myReturnFunction() {
-	document.getElementById("div1").style.WebkitTransition = "all 2s";
-	document.getElementById("div1").style.transition = "color 2s, background-color 2s, font-size 10s";
+//changes background color from yellow (make text smaller button) to a lightblue (return text to normal size) button
+function makeNormal() {
+	document.getElementById("change").style.WebkitTransition = "all 2s";
+	document.getElementById("change").style.transition = "color 2s, background-color 2s, font-size 10s";
 	document.body.style.transition = "background-color 2s";
 
-	document.getElementById("div1").style.color = "red";
-	document.getElementById("div1").style.backgroundColor = "lightBlue";
-	document.getElementById("div1").style.fontSize = "100%";
-
+	document.getElementById("change").style.color = "red"; //makes text color red
+	document.getElementById("change").style.backgroundColor = "lightBlue";
+	document.getElementById("change").style.fontSize = "100%"; //return text size back to normal
 	document.body.style.backgroundColor = "lightblue";
 }
 //END Topic #08 Triggering CSS3 with (Vanilla) JavaScript

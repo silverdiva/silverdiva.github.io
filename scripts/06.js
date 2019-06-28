8a273e898d7a60aaa01366d7309e51aa6eef9d3a
silverdiva <silverdiva4u@gmail.com>
2019-06-28 03:01:32 -0700

    updates to topic #6

---
+++ scripts/06.js
@@ -0,0 +1,68 @@
+//createElement(), appendChild() methods
+//<div id="create-and-append">
+function createImage() {
+	//creates img element
+	var image = document.createElement("IMG");
+	//sets img attributes
+	image.setAttribute("src", "rsz_pismo-mom-girls.jpg");
+	image.setAttribute("height", "75");
+	image.setAttribute("width", "90");
+	image.setAttribute("alt", "kim-and-girls");
+	//adds addtional img elements
+	document.getElementById("create-and-append").appendChild(image);
+}
+
+//insertBefore(), appendChild() methods
+//<div id="insertBefore-and-append">
+function insertBeforeElement() {
+	//creates a new button element
+	var newElement = document.createElement("BUTTON");
+	newElement.style.height = "25px"; 
+	newElement.style.width = "25%";
+	newElement.style.backgroundColor = "lightgray";
+	newElement.style.fontSize = "15px";
+
+	//creates text content displayed within the new button element
+	var newContent = document.createTextNode("Yay! Now I'm 2nd!");
+
+	//adds the new text content to the new button element 
+	newElement.appendChild(newContent);
+
+	//targets the div Id ("insertElem") where the new button element will be inserted 
+	var getDiv = document.getElementById("insertElem");
+
+	//adds the newly created "BUTTON" element ("var newElement") within the targeted div Id ("inserElem") and inserts it before the specified chld node index (".childNodes[2]")
+	getDiv.insertBefore(newElement, getDiv.childNodes[2]);
+}
+
+
+//removeChild(), appendChild() methods
+//<div id="remove-and-append">
+var element = null;
+
+function removeElement() {
+	if (element == null) {
+		//targets the div Id "myElement" to be removed
+		element = document.getElementById("myElement");
+		//removes the target element
+		element.parentNode.removeChild(element);
+	} else {
+		//popup window alert to add an element (when trying to remove a previously removed element) 
+		//cause you can't remove something that isn't there, right?
+		alert("Oh snap! Looks like you already removed the element. Add it back so you can remove it again!");
+	}
+}
+
+function insertElement() {
+	if (element) {
+		//targets the div Id "elemContainer" where the element will be inserted
+		var elemContainer = document.getElementById("elemContainer");
+		//adds element within the targeted Id
+		elemContainer.appendChild(element);
+		element = null;
+	} else {
+		//popup window alert to remove the element (when trying to add another element)
+		//cause you aren't able to add any more elements with this function - just the one!
+		alert("Oops! You can only add (1) element with this function. Remove it so you can add it again!");
+	}
+}
---
+++ scripts/09.js
@@ -0,0 +1,35 @@
+//change the border styling with onmouseover
+function borderChange() {
+	document.getElementById("kim-and-girls").style.border = "20px solid red";
+}
+//revert the border styling back to original with onmouseout 
+function borderChangeAgain() {
+	document.getElementById("kim-and-girls").style.border = "none";
+	document.getElementById("kim-and-girls").style.borderRadius = "10px";
+	document.getElementById("kim-and-girls").style.width = "50%";
+}
+
+
+
+//onlick changes image height to 75% of original size
+function doubleHeight() {
+	document.getElementById("kim-and-girls").style.width = "75%";
+}
+//onlick changes the image border radius
+function changeRadius() {
+	document.getElementById("kim-and-girls").style.borderRadius = "75px";
+}
+//onlick reverts image height back to original size and border radius
+function returnToNormal() {
+	document.getElementById("kim-and-girls").style.width = "60%";
+	document.getElementById("kim-and-girls").style.borderRadius = "30px";
+}
+//onclick changes the text color of ALL the buttons
+function changeColor() {
+	var color = prompt("To change the text color on the buttons, just pick a color - ANY color!");
+	var buttons = document.getElementsByClassName('myButtons');
+
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].style.color = color;
+	}
+}

e8dae129be6232268d708c2d78828999f4d08b84
silverdiva <silverdiva4u@gmail.com>
2019-07-13 21:58:30 -0700

    updated dev

---
+++ dev-hannahmacphotography/scripts/windchill.js
@@ -0,0 +1,11 @@
+//function myFunction2(){
+//var temp = (36 + 45)/2;
+//var speed= Math.pow(8,.16)
+//var answer = (35.74 + (0.6215*temp)) - (35.75*speed) + (0.4275*temp*speed)
+
+//document.getElementById("windChill").innerHTML = Math.round(answer);
+//}
+//myFunction2()
+
+//f=35.74 + 0.6215t - 35.75s^0.16 + 0.4275t s^0.16
+// t is the air average temerature fahrenheit and s is the wind speed in mph
